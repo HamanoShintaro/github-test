f7d8d5c5bb61f29865177f59fd79f813bc8f8cc5
濱野慎太朗 <hamano@joyworks.jp>
2019-08-23 12:58:57 +0900

    画面側修正(永井さん)

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { filter, map } from 'rxjs/operators';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 
 import { IIngredients } from 'app/shared/model/ingredients.model';
-import { IngredientsService } from './ingredients.service';
-import { LoginModalService, AccountService, Account } from 'app/core';
+import { IngredientsService } from '../entities/ingredients/ingredients.service';
 
 @Component({
     selector: 'jhi-home',
@@ -12,35 +13,34 @@ import { LoginModalService, AccountService, Account } from 'app/core';
     styleUrls: ['home.scss']
 })
 export class HomeComponent implements OnInit {
-    account: Account;
+    ingredients: IIngredients[];
     modalRef: NgbModalRef;
 
     constructor(
-        private accountService: AccountService,
-        private loginModalService: LoginModalService,
+        protected ingredientsService: IngredientsService,
+        protected jhiAlertService: JhiAlertService,
         private eventManager: JhiEventManager
     ) {}
 
     ngOnInit() {
-        this.accountService.identity().then((account: Account) => {
-            this.account = account;
-        });
-        this.registerAuthenticationSuccess();
+        this.loadAll();
     }
 
-    registerAuthenticationSuccess() {
-        this.eventManager.subscribe('authenticationSuccess', message => {
-            this.accountService.identity().then(account => {
-                this.account = account;
-            });
-        });
+    loadAll() {
+        this.ingredientsService
+            .query()
+            .pipe(
+                filter((res: HttpResponse<IIngredients[]>) => res.ok),
+                map((res: HttpResponse<IIngredients[]>) => res.body)
+            )
+            .subscribe(
+                (res: IIngredients[]) => {
+                    this.ingredients = res;
+                },
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
     }
-
-    isAuthenticated() {
-        return this.accountService.isAuthenticated();
-    }
-
-    login() {
-        this.modalRef = this.loginModalService.open();
+    protected onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
     }
 }
